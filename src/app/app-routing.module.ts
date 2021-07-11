797ac284f1792e424b3731f709d9016920400c33
gabrielagsoares <gabrielag.soares@otmail.com>
2021-07-11 19:07:05 -0300

    Atualização component logar e cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
 import { TemaComponent } from './tema/tema.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'logar', component: LogarComponent },
-  //{ path: 'cadastrar', component: CadastrarComponent },
+  { path: 'cadastrar', component: CadastrarComponent},
   { path: 'home', component: HomeComponent },
   { path: 'feed', component: FeedComponent},
   { path: 'tema' , component: TemaComponent}
--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario()
+  genero: string;
+  confirmarSenha: string;
+  tipoUsuario: string;
 
-  ngOnInit(): void {
+  constructor(
+    private authService : AuthService, //injeção de dependencia
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
   }
+
+  confirmSenha(event: any){
+    this.confirmarSenha = event.target.value
+  }
+
+  generos(event: any){
+    this.genero = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.genero = this.genero
+    this.usuario.tipoUser = this.tipoUsuario
+
+    if(this.usuario.senha != this.confirmarSenha){
+      alert('As senhas estão incorretas!')
+      console.log(this.usuario)
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario)=>{
+        this.usuario = resp
+        console.log(this.usuario)
+        this.router.navigate(['/logar'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
+  }
+
 
 }
